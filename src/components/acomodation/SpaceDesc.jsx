b7606444c99081f862a345f2048f4dae0f80e901
Francisco Neto <wantedshark@gmail.com>
2022-09-30 15:47:12 -0300

    space and description component created

--- src/components/acomodation/SpaceDesc.jsx
+++ src/components/acomodation/SpaceDesc.jsx
@@ -1,11 +1,66 @@
-import { X } from "phosphor-react";
+import { MinusCircle, PlusCircle, X } from "phosphor-react";
+import { useState } from "react";
 
-export default function SpaceDesc() {
-    return(
-        <div>
-            <header>
-                <X size={20} />
-            </header>
+export default function SpaceDesc({ setSpaceDescVisib }) {
+    const [guests, setGuests] = useState(4)
+    const [toggleSpaceType, setToggleSpaceType] = useState(false)
+    const [wholeSpace, setWholeSpace] = useState(false)
+    const [wholeBedroom, setWholeBedroom] = useState(false)
+
+    function handleSelectWholeSpace() {
+        setWholeSpace(!wholeSpace)
+        setGuests(4)
+        setToggleSpaceType(!toggleSpaceType)
+    }
+    function handleSelectWholeBedroom() {
+        setWholeBedroom(!wholeBedroom)
+        setGuests(2)
+        setToggleSpaceType(!toggleSpaceType)
+    }
+    function handleAddValue() {
+        if (guests < 16) {
+            setGuests(guests + 1)
+        }
+    }
+    function handleDecreaseValue() {
+        if (guests > 1) {
+            setGuests(guests - 1)
+        }
+    }
+    return (
+        <div className="spaceDesc-container position-fixed start-0 top-0 container-fluid" style={{ height: "100vh" }}>
+            <div className="position-fixed start-0 bottom-0 bg-light container-fluid rounded-top" style={{ height: "80vh" }}>
+                <header className="position-relative pt-3 bottom-border">
+                    <X size={20} onClick={() => setSpaceDescVisib(false)} className="position-absolute" />
+                    <p className="text-center fw-bold">Descreva seu espaço</p>
+                </header>
+                <main>
+                    <div className="addressAndRegion bottom-border py-5">
+                        <label className="form-label" htmlFor="address">Endereço ou região</label>
+                        <input id="address" type="text" className="form-control" />
+                    </div>
+                    <div className="space-type bottom-border py-5">
+                        <label htmlFor="space-inpt">Tipo de espaço</label>
+                        <div className="space-type-button-container d-flex justify-content-evenly p-2 rounded-5 mt-4">
+                            <button className={toggleSpaceType ? "font-smaller bg-transparent border-0 rounded-4 p-1 px-2 fw-bold" : "font-smaller bg-light border-0 rounded-4 p-1 px-2 fw-bold"} onClick={handleSelectWholeSpace}>Espaço inteiro</button>
+                            <button className={toggleSpaceType ? "font-smaller bg-light border-0 rounded-4 p-1 px-2 fw-bold" : "font-smaller bg-transparent border-0 rounded-4 p-1 px-2 fw-bold"} onClick={handleSelectWholeBedroom}>Quarto inteiro</button>
+                        </div>
+                    </div>
+                    <div className="guests-container d-flex justify-content-between py-4 px-2 bottom-border">
+                        <p className="mt-1 fw-bold">Hóspedes</p>
+                        <div className="guests-buttons">
+                            <MinusCircle size={30} onClick={handleDecreaseValue} />
+                            <span className="mx-1">
+                                {guests}
+                            </span>
+                            <PlusCircle size={30} onClick={handleAddValue} />
+                        </div>
+                    </div>
+                    <div className="container d-flex justify-content-center mt-5">
+                        <button className="btn btn-dark text-light px-4 py-2">Atualize sua estimativa</button>
+                    </div>
+                </main>
+            </div>
         </div>
     )
 }
--- src/pages/AcomodationHost.jsx
+++ src/pages/AcomodationHost.jsx
@@ -1,10 +1,13 @@
 import { CaretLeft, MagnifyingGlass } from "phosphor-react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { showNav } from "../assets/appSlice";
+import SpaceDesc from "../components/acomodation/SpaceDesc"
 export default function AcomodationHost() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const [spaceDescVisib, setSpaceDescVisib] = useState(false)
     function handleBackButton() {
         navigate(-1)
         dispatch(showNav())
@@ -13,9 +16,9 @@ export default function AcomodationHost() {
         <>
             <header className="posiiton-relative">
                 <CaretLeft size={20} className="position-absolute mt-2 ms-2 text-dark" onClick={handleBackButton}/>
-                <img src="https://www.meusonhar.com.br/wp-content/uploads/2016/11/casa-nova-sonhar-com.jpg" alt="" style={{width:"100vw"}} />
+                <img src="https://www.meusonhar.com.br/wp-content/uploads/2016/11/casa-nova-sonhar-com.jpg" alt="" style={{width:"100%"}} />
             </header>
-            <main className="mt-5">
+            <main className={!spaceDescVisib ? "mt-5" : "mt-5 scroll-lock"}>
                 <h1 className="ms-4 me-5 mb-5 fw-bolder">Abra suas portas para receber hóspedes</h1>
                 <h2 className="ms-4 me-5 mb-4 fw-bolder fs-3">Descubra quanto você pode ganhar como anfitrião</h2>
                 <section className="region-receipt p-4">
@@ -36,14 +39,31 @@ export default function AcomodationHost() {
                 </section>
                 <section className="space-desc container-fluid position-relative p-2">
                     <p className="ms-3 fw-bold">Descreva o seu espaço</p>
-                    <button className=" border-0 rounded-5 px-5 position-absolute ms-4 mt-2 bg-light" style={{ width: "80%" }}>
+                    <button className=" border-0 rounded-5 px-5 position-absolute ms-4 mt-2 bg-light" style={{ width: "80%" }} onClick={() => setSpaceDescVisib(!spaceDescVisib)}>
                         <span>
                             <MagnifyingGlass size={20} className="text-danger position-absolute start-0 ms-2" />
                             Beberibe</span>
                     </button>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31834.12237109596!2d-38.14479012723916!3d-4.168168852770807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b88e0b58561925%3A0xd03dac14ccee1cc4!2sBeberibe%2C%20CE%2C%2062840-000!5e0!3m2!1spt-BR!2sbr!4v1664545325168!5m2!1spt-BR!2sbr" width="100%" height="300" style={{border:0}} allowfFllScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+                    {spaceDescVisib && <SpaceDesc setSpaceDescVisib={setSpaceDescVisib} />}
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31834.12237109596!2d-38.14479012723916!3d-4.168168852770807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b88e0b58561925%3A0xd03dac14ccee1cc4!2sBeberibe%2C%20CE%2C%2062840-000!5e0!3m2!1spt-BR!2sbr!4v1664545325168!5m2!1spt-BR!2sbr" width="100%" height="300" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </section>
+                <section className="my-5">
+                    <h2 className="mb-4 ms-3 fw-bold fs-3">Você pode hospedar onde quiser, quando quiser</h2>
+                    <div className="anfitriao-resume-container d-flex">
+                        <div className="anfitriao-resume d-flex flex-column align-items-center p-2">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScuOKcRCySbtTZHLPP2FT8nvNHG25StGcdhw&usqp=CAU" alt="" className="rounded-3" style={{height:"250px"}}/>
+                            <p className="mt-4">Hospedar meu chalé me deu uma renda a mais e me possibilitou investir em novos projetos</p>
+                        </div>
+                        <div className="anfitriao-resume d-flex flex-column align-items-center p-2">
+                            <img src="https://storage.alboom.ninja/sites/2851/albuns/377747/foto_perfil_profissional_medico_consultorio_hospital_albert_einstein-1.jpg?t=1548277552" alt="" className="rounded-3" style={{height:"250px", width:"190px"}}/>
+                            <p className="mt-4">Compartilhar um quarto mudou minha vida, conheci pessoas novas tive experiências incriveis.</p>
+                        </div>
+                    </div>
+                </section>
+                <div className="bg-dark text-light mb-5 p-4 rounded-top d-flex flex-column justify-content-center" style={{height:"250px"}}>
+                    <h2 className="mb-4">Experimente ser anfitrião no mybnb</h2>
+                    <p>Faça parte da comunidade. Ajudaremos você em cada etapa.</p>
+                </div>
                 <div className="acomodation-experience container-fluid p-2 position-fixed bottom-0 bg-light d-flex justify-content-center border-top border-secondary">
                     <button className="btn btn-danger me-2">Experimente Hospedar</button>
                 </div>
