9a5f7b847290d56dbff2e40d1bc90781d003623f
Francisco Neto <wantedshark@gmail.com>
2022-10-05 14:53:15 -0300

    conveniences step added

--- src/components/acomodation/AddAcomodation.jsx
+++ src/components/acomodation/AddAcomodation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { X, MinusCircle, PlusCircle } from "phosphor-react";
 import { useState } from "react";
-import { GoogleMap, Marker  } from "@react-google-maps/api";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 import AutocompleteElem from "./AutocompleteElem";
 
 
@@ -16,7 +16,9 @@ export default function AddAcomodation({ handleToggleAcmdVisib }) {
         lat: -4.179914,
         lng: -38.129617
     })
-
+    const conveniences = ["piscina", "Churrasqueira", "jacuzzi", "patio", "fogueira", "Área de lazer"]
+    const preferences = ["Wi-Fi", "Tv", "Cozinha", "maquina de lavar", "chuveiro eletrico", "Ar-condicionado"]
+    const securityItems = ["Alarme", "extintor de incendio", "Detector de fumaças", "kit de primeiros socorros"]
     const spaceType = ["Apartamento", "Casa", "Unidade-secundária", "Acomodação única", "Pousada", "Hotel botique"]
     const spaceTypesDesc = [
         {
@@ -55,7 +57,7 @@ export default function AddAcomodation({ handleToggleAcmdVisib }) {
     const placeType = ["Lugar inteiro", "Um quarto inteiro", "Um quarto compartilhado"]
 
     function handleNextStep() {
-        if (step < 6) {
+        if (step < 7) {
             setStep(step + 1)
         }
     }
@@ -64,10 +66,32 @@ export default function AddAcomodation({ handleToggleAcmdVisib }) {
             setStep(step - 1)
         }
     }
+    function handleIncrease(func, elem) {
+        if (elem === hostsQuant && hostsQuant < 16) {
+            func(elem + 1)
+        } else if (elem === bedsQuant && bedsQuant < 14) {
+            func(elem + 1)
+        } else if (elem === bedroomsQuant && bedroomsQuant < 8) {
+            func(elem + 1)
+        } else if (elem === bethroomsQuant && bethroomsQuant < 8) {
+            func(elem + 1)
+        }
+    }
+    function handleDecrease(func, elem) {
+        if (elem === hostsQuant && hostsQuant > 1) {
+            func(elem - 1)
+        } else if (elem === bedsQuant && bedsQuant > 1) {
+            func(elem - 1)
+        } else if (elem === bedroomsQuant && bedroomsQuant > 1) {
+            func(elem - 1)
+        } else if (elem === bethroomsQuant && bethroomsQuant > 1) {
+            func(elem - 1)
+        }
+    }
     return (
         <div className="addAcomodation">
             <header className="addAcomodation-header py-5" style={{ height: "160px" }}>
-                <X size={25} className="close-addAcmd-btn position-absolute top-0 m-2 rounded-5 p-1 text-light" onClick={handleToggleAcmdVisib} />
+                <X size={25} className="close-addAcmd-btn position-absolute top-0 ms-3 mt-3 rounded-5 p-1 text-light" onClick={handleToggleAcmdVisib} />
                 <h1 className="AddAcomodation-tittle text-light fs-2 me-4 ms-4 mt-4">Em que tipo de espaço você vai hospedar?</h1>
                 <nav className="position-fixed bottom-0 container d-flex justify-content-between p-2">
                     <button className="bg-transparent border-0 text-decoration-underline fw-bold" onClick={handleBackStep}>Voltar</button>
@@ -114,30 +138,63 @@ export default function AddAcomodation({ handleToggleAcmdVisib }) {
                 }
                 {step === 5 &&
                     <div className="hosts">
-                        <div>
+                        <div className="d-flex justify-content-between p-2 my-2 border rounded-3">
                             Hóspedes
-                            <MinusCircle />
-                            {hostsQuant}
-                            <PlusCircle />
+                            <div className="buttons">
+                                <MinusCircle className={hostsQuant === 1 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleDecrease(setHostsQuant, hostsQuant)} />
+                                {hostsQuant}
+                                <PlusCircle className={hostsQuant === 16 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleIncrease(setHostsQuant, hostsQuant)} />
+                            </div>
                         </div>
-                        <div>
+                        <div className="d-flex justify-content-between p-2 my-2 border rounded-3">
                             Camas
-                            <MinusCircle />
-                            {bedsQuant}
-                            <PlusCircle />
+                            <div className="buttons">
+                                <MinusCircle className={bedsQuant === 1 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleDecrease(setBedsQuant, bedsQuant)} />
+                                {bedsQuant}
+                                <PlusCircle className={bedsQuant === 14 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleIncrease(setBedsQuant, bedsQuant)} />
+                            </div>
                         </div>
-                        <div>
+                        <div className="d-flex justify-content-between p-2 my-2 border rounded-3">
                             Quartos
-                            <MinusCircle />
-                            {bedroomsQuant}
-                            <PlusCircle />
+                            <div className="buttons">
+                                <MinusCircle className={bedroomsQuant === 1 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleDecrease(setBedroomsQuant, bedroomsQuant)} />
+                                {bedroomsQuant}
+                                <PlusCircle className={bedroomsQuant === 8 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleIncrease(setBedroomsQuant, bedroomsQuant)} />
+                            </div>
                         </div>
-                        <div>
+                        <div className="d-flex justify-content-between p-2 my-2 border rounded-3">
                             Banheiros
-                            <MinusCircle />
-                            {bethroomsQuant}
-                            <PlusCircle />
+                            <div className="buttons">
+                                <MinusCircle className={bethroomsQuant === 1 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleDecrease(setBethroomsQuant, bethroomsQuant)} />
+                                {bethroomsQuant}
+                                <PlusCircle className={bethroomsQuant === 8 ? "mx-1 border-secondary text-secondary" : "mx-1"} size={22} onClick={() => handleIncrease(setBethroomsQuant, bethroomsQuant)} />
+                            </div>
                         </div>
+                    </div>
+                }
+                {step === 6 &&
+                    <div className="conveniences-container mb-5">
+                        <h2 className="fs-4 fw-bold">Você tem alguma comodidate que mereça destaque ?</h2>
+                        <div className="conveniences container d-flex flex-wrap justify-content-evenly my-3">
+                            {conveniences.map((conv, i) =>
+                                <button key={i} className="conv-items my-2" onClick={e => e.target.classList.toggle("selected")}>{conv}</button>)}
+                        </div>
+                        <h2 className="fs-4 fw-bold">E estes favoritos dos hospedes ?</h2>
+                        <div className="preferences container d-flex flex-wrap justify-content-evenly my-3">
+                            {preferences.map((pref, i) =>
+                                <button key={i} className="pref-items my-2" onClick={e => e.target.classList.toggle("selected")}>{pref}</button>)}
+                        </div>
+                        <h2 className="fs-4 fw-bold">Tem algum desses itens de segurança?</h2>
+                        <div className="security-items container d-flex flex-wrap justify-content-evenly my-3">
+                            {securityItems.map((secItem, i) =>
+                                <button key={i} className="sec-items my-2" onClick={e => e.target.classList.toggle("selected")}>{secItem}</button>)}
+                        </div>
+                    </div>
+                }
+                {step === 7 &&
+                    <div className="images">
+                        <img src="" alt="" />
+                        <h2 className="fs-4">Agora vamos adicionar algumas fotos</h2>
                     </div>
                 }
             </main>
